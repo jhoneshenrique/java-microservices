b448ff10eec2d9c746e77f281b48fda5b702a488
Jhones Henrique <jhoneshenriquedasilva@gmail.com>
2022-10-31 22:11:21 -0300

    Inventory and Order communication using WebClient

--- inventory-service/src/main/java/com/jhsoftware/invetoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/jhsoftware/invetoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.jhsoftware.invetoryservice.controller;
 
+import com.jhsoftware.invetoryservice.dto.InventoryResponse;
 import com.jhsoftware.invetoryservice.service.InvenventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/inventory")
@@ -13,9 +16,9 @@ public class InventoryController {
     private final InvenventoryService invenventoryService;
 
     //It receives a skucode and check if it is in stock
-    @GetMapping("/sku-code")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return invenventoryService.isInStock(skuCode);
     }
 }
--- order-service/src/main/java/com/jhsoftware/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/jhsoftware/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.jhsoftware.orderservice.service;
 
+import com.jhsoftware.orderservice.dto.InventoryResponse;
 import com.jhsoftware.orderservice.dto.OrderLineItemsDto;
 import com.jhsoftware.orderservice.dto.OrderRequest;
 import com.jhsoftware.orderservice.model.Order;
@@ -7,15 +8,23 @@ import com.jhsoftware.orderservice.model.OrderLineItems;
 import com.jhsoftware.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
     //Order Repository Dependency Injection
     private final OrderRepository orderRepository;
+
+    //WebClient dependency injection
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -29,8 +38,28 @@ public class OrderService {
         //Set the orderlineitems to the order
         order.setOrderLineItemsList(orderLineItems);
 
-        //Saving the order
-        orderRepository.save(order);
+        //Get all the skucodes of the products
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+
+        //Call Inventory service, and place order if product is in stock
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder
+                        .queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class) //retrieve the body of the info.
+                .block(); //That tell us is a syncronous call.
+
+        //Check if the items inside the array exists
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock){
+            //Saving the order
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock!");
+        }
+
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
