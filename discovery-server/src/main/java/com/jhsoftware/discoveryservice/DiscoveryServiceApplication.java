6a08d2becfcc76104df034f6c3d025ec732e0e77
Jhones Henrique <jhoneshenriquedasilva@gmail.com>
2022-11-06 11:30:55 -0300

    Security with keycloack

--- discovery-server/src/main/java/com/jhsoftware/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-server/src/main/java/com/jhsoftware/discoveryservice/DiscoveryServiceApplication.java
@@ -3,11 +3,14 @@ package com.jhsoftware.discoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaServer //Transform it into an Eureka server
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(DiscoveryServiceApplication.class, args);
+        System.out.println(new BCryptPasswordEncoder().encode("admin"));
     }
 }
